58f917ea1cda13a3c1f11d87687cbfe54f102c03
austin98x <wcfds@sina.com>
2016-02-17 14:55:44 +0800

    add predefined macros for UNIX platform

--- include/bfc/predef/pre_def.h
+++ include/bfc/predef/pre_def.h
@@ -29,101 +29,11 @@
 #if defined(X_OS_WINDOWS)
 #include <windows.h>
 #else
+#include "pre_unixdef.h"
 #endif
 
 // -------------------------------------------------------------------------- //
 // --> Keyword
-#undef far
-#undef near
-#define far
-#define near
-#undef FAR
-#undef NEAR
-#define FAR                 far
-#define NEAR                near
-
-#ifndef CDECL
-#define CDECL	__cdecl
-#endif
-
-#ifndef PASCAL
-#define PASCAL	__stdcall
-#endif
-
-#ifndef CONST
-#define CONST               const
-#endif
-
-#ifndef interface
-#define interface struct
-#endif
-
-#ifndef EXTERN_C
-#ifdef __cplusplus
-#define EXTERN_C    extern "C"
-#else
-#define EXTERN_C
-#endif
-#endif
-
-#ifndef _HRESULT_DEFINED
-#define _HRESULT_DEFINED
-typedef INT32 HRESULT;
-#endif // !_HRESULT_DEFINED
-
-#ifndef STDAPI
-#define STDAPI				EXTERN_C __export HRESULT __stdcall
-#define STDAPI_(type)       EXTERN_C __export type __stdcall
-
-#define STDAPICALLTYPE                  __stdcall
-#define STDAPIVCALLTYPE                 __cdecl
-#endif
-
-#ifndef EXPORTAPI
-#define EXPORTAPI			EXTERN_C __export HRESULT __stdcall
-#define EXPORTAPI_(type)	EXTERN_C __export type __stdcall
-#endif
-
-#ifndef STDMETHOD
-#define STDMETHODCALLTYPE		__stdcall
-#define STDMETHOD(method)       virtual HRESULT STDMETHODCALLTYPE method
-#define STDMETHOD_(type,method) virtual type STDMETHODCALLTYPE method
-#define STDMETHODIMP            HRESULT STDMETHODCALLTYPE
-#define STDMETHODIMP_(type)     type STDMETHODCALLTYPE
-#endif
-
-#ifndef PURE
-#define PURE					= 0
-#endif
-
-#ifndef STDPROC
-#define STDPROC					virtual HRESULT __stdcall
-#define STDPROC_(Type)			virtual Type __stdcall
-#endif
-
-#ifndef APIENTRY
-#define APIENTRY				__stdcall
-#endif
-
-#ifndef WINAPI
-#define WINAPI					__stdcall
-#endif
-
-#ifndef CALLBACK
-#define CALLBACK				__stdcall
-#endif
-
-#ifndef DECLARE_HANDLE
-	typedef void* HANDLE;
-	#define DECLARE_HANDLE(name) struct __handle_##name##__ { int unused; }; typedef struct name##__ *name
-	typedef HANDLE* PHANDLE;
-#endif
-
-#if !defined(__HMODULE)
-#define __HMODULE
-	DECLARE_HANDLE(HINSTANCE);
-	typedef HINSTANCE HMODULE;	/* HMODULEs can be used in place of HINSTANCEs */
-#endif
 
 #if !defined(__BEGIN_DECLS)
 #	if defined(__cplusplus)
@@ -136,37 +46,7 @@ typedef INT32 HRESULT;
 #endif
 
 // -------------------------------------------------------------------------- //
-// --> Type Define
-#undef TRUE
-#undef FALSE
-#define TRUE				1
-#define FALSE				0
-
-#ifndef BASETYPES
-#define BASETYPES
-typedef long LONG;
-typedef LONG *PLONG;
-typedef unsigned long ULONG;
-typedef ULONG *PULONG;
-typedef short SHORT;
-typedef SHORT *PSHORT;
-typedef unsigned short USHORT;
-typedef USHORT *PUSHORT;
-typedef char CHAR;
-typedef CHAR *PCHAR;
-typedef unsigned char UCHAR;
-typedef UCHAR *PUCHAR;
-typedef char *PSZ;
-#endif  // !BASETYPES
-
-#ifndef VOID
-#define VOID void
-#endif
-
-typedef void* PVOID;
-typedef unsigned short VARTYPE;
-typedef short VARIANT_BOOL;
-typedef long SCODE;
+// --> Type Definitions
 
 typedef int8_t				int8;
 typedef int16_t				int16;
@@ -187,34 +67,6 @@ typedef int32				INT32;
 typedef uint32				UINT32;
 typedef int64				INT64;
 typedef uint64				UINT64;
-typedef UINT32				DWORD;
-typedef int32				BOOL;
-typedef unsigned char		BYTE;
-typedef uint16				WORD;
-typedef float				FLOAT;
-typedef double				DOUBLE;
-typedef FLOAT				*PFLOAT;
-typedef BOOL near			*PBOOL;
-typedef BOOL far			*LPBOOL;
-typedef BYTE near			*PBYTE;
-typedef BYTE far			*LPBYTE;
-typedef int near			*PINT;
-typedef int far				*LPINT;
-typedef WORD near			*PWORD;
-typedef WORD far			*LPWORD;
-typedef long far			*LPLONG;
-typedef DWORD near			*PDWORD;
-typedef DWORD far			*LPDWORD;
-typedef void far			*LPVOID;
-typedef CONST void far		*LPCVOID;
-
-typedef int					INT;
-typedef unsigned int		UINT;
-typedef unsigned int		*PUINT;
-typedef long long			LONGLONG;
-typedef unsigned long long	ULONGLONG;
-typedef DWORD				LCID;
-typedef DWORD				LCTYPE;
 
 typedef uintp				UINTP;
 typedef intp				INTP;
@@ -223,9 +75,6 @@ typedef int32				size32;
 typedef uint32				usize32;
 typedef size32				SIZE32;
 typedef usize32				USIZE32;
-
-typedef DWORD   COLORREF;
-typedef DWORD*	LPCOLORREF;
 
 static_assert(sizeof(DWORD) == 4, "DWORD has no 4 bytes.");
 static_assert(sizeof(int8) == 1, "int8 has no 1 byte");
@@ -239,81 +88,8 @@ static_assert(sizeof(uint64) == 8, "uint64 has no 8 byte");
 static_assert(sizeof(intp) == sizeof(void*), "intp's size not equal to void *");
 static_assert(sizeof(uintp) == sizeof(void*), "uintp's size not equal to void *");
 
-typedef unsigned short WCHAR;
-
-typedef WCHAR*	PWCHAR;
-typedef WCHAR*	LPWCH, *PWCH;
-typedef WCHAR*	LPWSTR, *PWSTR;
-typedef CONST WCHAR* LPCWCHAR, *PCWCHAR;
-typedef CONST WCHAR* LPCWCH, *PCWCH;
-typedef CONST WCHAR* LPCWSTR, *PCWSTR;
-
-typedef LPCWSTR LPCOLESTR;
-typedef LPWSTR LPOLESTR;
-
-typedef CHAR *PCHAR;
-typedef CHAR *LPCH, *PCH;
-
-typedef CONST CHAR *LPCCH, *PCCH;
-typedef CHAR *NPSTR;
-typedef CHAR *LPSTR, *PSTR;
-typedef CONST CHAR *LPCSTR, *PCSTR;
-
-#ifndef VARIANT_TRUE
-#define VARIANT_TRUE	((VARIANT_BOOL)0xffff)
-#define VARIANT_FALSE	((VARIANT_BOOL)0)
-#endif
-
-
-// --> Neutral ANSI/UNICODE types and macros
-#ifdef  UNICODE
-
-#ifndef _TCHAR_DEFINED
-#define _TCHAR_DEFINED
-typedef WCHAR TCHAR, *PTCHAR;
-typedef WCHAR TBYTE, *PTBYTE;
-#endif
-
-typedef LPWSTR LPTCH, PTCH;
-typedef LPWSTR PTSTR, LPTSTR;
-typedef LPCWSTR LPCTSTR;
-typedef LPWSTR LP;
-#define __TEXT(quote) L##quote
-
-#else   /* UNICODE */
-
-#ifndef _TCHAR_DEFINED
-#define _TCHAR_DEFINED
-typedef char TCHAR, *PTCHAR;
-typedef unsigned char TBYTE, *PTBYTE ;
-#endif /* !_TCHAR_DEFINED */
-
-typedef LPSTR LPTCH, PTCH;
-typedef LPSTR PTSTR, LPTSTR;
-typedef LPCSTR LPCTSTR;
-#define __TEXT(quote) quote
-
-#endif /* UNICODE */
-
-
 // -------------------------------------------------------------------------- //
 // --> Macros
-
-#define MAKEWORD(a, b)      ((WORD)(((BYTE)(a)) | ((WORD)((BYTE)(b))) << 8))
-#define MAKEINT32(a, b)     ((INT32)(((WORD)(a)) | ((DWORD)((WORD)(b))) << 16))
-#define LOWORD(l)           ((WORD)(l))
-#define HIWORD(l)           ((WORD)(((DWORD)(l) >> 16) & 0xFFFF))
-#define LOBYTE(w)           ((BYTE)(w))
-#define HIBYTE(w)           ((BYTE)(((WORD)(w) >> 8) & 0xFF))
-
-#ifndef RGB
-#define RGB(r,g,b)		((COLORREF)(((BYTE)(r)|((WORD)((BYTE)(g))<<8))|(((DWORD)(BYTE)(b))<<16)))
-#endif
-
-#ifndef LISet32
-#define LISet32(li, v)		((li).HighPart = (v) < 0 ? -1 : 0, (li).LowPart = (v))
-#define ULISet32(li, v)		((li).HighPart = 0, (li).LowPart = (v))
-#endif
 
 #ifdef X_CC_VC
 #define __X(x) L ## x
@@ -322,27 +98,6 @@ typedef LPCSTR LPCTSTR;
 #define __X(x) ((unsigned short *)(u ## x))
 #define OLESTR(x) __X(x)
 #define __Xc(x) ((unsigned short)(u ## x))
-#endif
-
-#ifndef	countof
-#  define countof(array)						(sizeof(array)/sizeof(array[0]))
-#endif
-
-#ifndef _countof
-#  define _countof(array)						(sizeof(array)/sizeof(array[0]))
-#endif
-
-#ifndef _offsetof
-#	if defined(X_CC_GCC)
-#		define _offsetof(coclass, member)	( (size_t)&((coclass*)64)->member - 64 )
-#	else
-#		define _offsetof(coclass, member)	( (size_t)&((coclass*)0)->member )
-#	endif
-#endif
-
-#ifndef parent_class_ptr
-#define parent_class_ptr(ParentClass, member)	\
-	( (ParentClass*)((const char*)(this) - _offsetof(ParentClass, member)) )
 #endif
 
 // -------------------------------------------------------------------------- //
---
+++ include/bfc/predef/pre_unixdef.h
@@ -0,0 +1,304 @@
+// -------------------------------------------------------------------------- //
+//	File name		£º	pre_unixdef.h
+//	Author 			£º	Austin
+//	Create Time		£º	2016-2-17 11:38:45
+//	Description 	£º	Predefined macros for UNIX platform
+//
+// -------------------------------------------------------------------------- //
+
+#ifndef __PRE_UNIXDEF_H__
+#define __PRE_UNIXDEF_H__
+
+#ifndef __BFC_INCLUDE__
+#	error This file is not intended to be included separately
+#endif
+
+// -------------------------------------------------------------------------- //
+// --> Type Definitions
+
+#ifndef BASETYPES
+#define BASETYPES
+typedef unsigned long ULONG;
+typedef ULONG *PULONG;
+typedef unsigned short USHORT;
+typedef USHORT *PUSHORT;
+typedef unsigned char UCHAR;
+typedef UCHAR *PUCHAR;
+typedef char *PSZ;
+#endif  /* !BASETYPES */
+
+#ifndef VOID
+#define VOID void
+typedef char CHAR;
+typedef short SHORT;
+typedef long LONG;
+typedef int INT;
+#endif
+
+#define MAX_PATH          260
+
+#ifndef NULL
+#ifdef __cplusplus
+#define NULL    0
+#else
+#define NULL    ((void *)0)
+#endif
+#endif
+
+#ifndef FALSE
+#define FALSE               0
+#endif
+
+#ifndef TRUE
+#define TRUE                1
+#endif
+
+#ifndef IN
+#define IN
+#endif
+
+#ifndef OUT
+#define OUT
+#endif
+
+#ifndef OPTIONAL
+#define OPTIONAL
+#endif
+
+#undef far
+#undef near
+#undef pascal
+#define far
+#define near
+#define pascal __stdcall
+
+#undef FAR
+#undef  NEAR
+#define FAR                 far
+#define NEAR                near
+#ifndef CONST
+#define CONST               const
+#endif
+
+#ifndef CDECL
+#define CDECL _cdecl
+#endif
+
+#ifndef PASCAL
+#define PASCAL __stdcall
+#endif
+
+#ifndef CALLBACK
+#define CALLBACK    __stdcall
+#endif
+
+#ifndef WINAPI
+#define WINAPI      __stdcall
+#endif
+
+#ifndef APIENTRY
+#define APIENTRY    __stdcall
+#endif
+
+
+typedef unsigned int        DWORD;
+typedef int                 BOOL;
+typedef unsigned char       BYTE;
+typedef unsigned short      WORD;
+typedef float               FLOAT;
+typedef double              DOUBLE;
+typedef FLOAT               *PFLOAT;
+typedef BOOL near           *PBOOL;
+typedef BOOL far            *LPBOOL;
+typedef BYTE near           *PBYTE;
+typedef BYTE far            *LPBYTE;
+typedef int near            *PINT;
+typedef int far             *LPINT;
+typedef WORD near           *PWORD;
+typedef WORD far            *LPWORD;
+typedef long far            *LPLONG;
+typedef DWORD near          *PDWORD;
+typedef DWORD far           *LPDWORD;
+typedef void far            *LPVOID;
+typedef CONST void far      *LPCVOID;
+
+typedef int                 INT;
+typedef unsigned int        UINT;
+typedef unsigned int        *PUINT;
+typedef long long           LONGLONG;
+typedef unsigned long long  ULONGLONG;
+
+typedef unsigned short WCHAR;
+typedef WCHAR* PWCHAR;
+typedef WCHAR* LPWCH, *PWCH;
+typedef WCHAR* LPWSTR, *PWSTR;
+typedef CONST WCHAR* LPCWCHAR, *PCWCHAR;
+typedef CONST WCHAR* LPCWCH, *PCWCH;
+typedef CONST WCHAR* LPCWSTR, *PCWSTR;
+typedef LPCWSTR LPCOLESTR;
+typedef LPWSTR LPOLESTR;
+
+typedef CHAR *PCHAR;
+typedef CHAR *LPCH, *PCH;
+typedef CONST CHAR *LPCCH, *PCCH;
+typedef CHAR *NPSTR;
+typedef CHAR *LPSTR, *PSTR;
+typedef CONST CHAR *LPCSTR, *PCSTR;
+
+// --> Neutral ANSI/UNICODE types and macros
+#ifdef  UNICODE
+
+#ifndef _TCHAR_DEFINED
+#define _TCHAR_DEFINED
+typedef WCHAR TCHAR, *PTCHAR;
+typedef WCHAR TBYTE, *PTBYTE;
+#endif /* !_TCHAR_DEFINED */
+
+typedef LPWSTR LPTCH, PTCH;
+typedef LPWSTR PTSTR, LPTSTR;
+typedef LPCWSTR LPCTSTR;
+#define __TEXT(quote) L##quote
+
+#else   /* UNICODE */
+
+#ifndef _TCHAR_DEFINED
+#define _TCHAR_DEFINED
+typedef char TCHAR, *PTCHAR;
+typedef unsigned char TBYTE, *PTBYTE ;
+#endif /* !_TCHAR_DEFINED */
+
+typedef LPSTR LPTCH, PTCH;
+typedef LPSTR PTSTR, LPTSTR;
+typedef LPCSTR LPCTSTR;
+#define __TEXT(quote) quote
+
+#endif /* UNICODE */
+
+#ifndef VARIANT_TRUE
+#define VARIANT_TRUE    ((VARIANT_BOOL)0xffff)
+#define VARIANT_FALSE   ((VARIANT_BOOL)0)
+#endif
+
+typedef DWORD   COLORREF;
+typedef DWORD   *LPCOLORREF;
+
+#define MAKEWORD(a, b)      ((WORD)(((BYTE)(a)) | ((WORD)((BYTE)(b))) << 8))
+#define MAKEINT32(a, b)     ((INT32)(((WORD)(a)) | ((DWORD)((WORD)(b))) << 16))
+#define LOWORD(l)           ((WORD)(l))
+#define HIWORD(l)           ((WORD)(((DWORD)(l) >> 16) & 0xFFFF))
+#define LOBYTE(w)           ((BYTE)(w))
+#define HIBYTE(w)           ((BYTE)(((WORD)(w) >> 8) & 0xFF))
+
+#ifndef RGB
+#define RGB(r,g,b)		((COLORREF)(((BYTE)(r)|((WORD)((BYTE)(g))<<8))|(((DWORD)(BYTE)(b))<<16)))
+#endif
+
+typedef void* HANDLE;
+#define DECLARE_HANDLE(name) struct __handle_##name##__ { int unused; }; typedef struct name##__ *name
+typedef HANDLE* PHANDLE;
+DECLARE_HANDLE(HINSTANCE);
+typedef HINSTANCE HMODULE;
+
+typedef struct tagRECT
+{
+	LONG    left;
+	LONG    top;
+	LONG    right;
+	LONG    bottom;
+} RECT, *PRECT, NEAR *NPRECT, FAR *LPRECT;
+
+typedef const RECT FAR* LPCRECT;
+
+typedef struct _RECTL
+{
+	LONG    left;
+	LONG    top;
+	LONG    right;
+	LONG    bottom;
+} RECTL, *PRECTL, *LPRECTL;
+
+typedef const RECTL FAR* LPCRECTL;
+
+typedef struct tagPOINT
+{
+	LONG  x;
+	LONG  y;
+} POINT, *PPOINT, NEAR *NPPOINT, FAR *LPPOINT;
+
+typedef struct _POINTL
+{
+	LONG  x;
+	LONG  y;
+} POINTL, *PPOINTL;
+
+typedef struct tagSIZE
+{
+	LONG        cx;
+	LONG        cy;
+} SIZE, *PSIZE, *LPSIZE;
+
+typedef SIZE               SIZEL;
+typedef SIZE               *PSIZEL, *LPSIZEL;
+
+typedef struct tagPOINTS
+{
+	SHORT   y;
+	SHORT   x;
+} POINTS, *PPOINTS, *LPPOINTS;
+
+typedef struct _FILETIME {
+	DWORD dwLowDateTime;
+	DWORD dwHighDateTime;
+} FILETIME, *PFILETIME, *LPFILETIME;
+
+#ifndef _HRESULT_DEFINED
+#define _HRESULT_DEFINED
+typedef int HRESULT;
+#endif // !_HRESULT_DEFINED
+
+#ifndef EXTERN_C
+#ifdef __cplusplus
+#define EXTERN_C extern "C"
+#else
+#define EXTERN_C
+#endif
+#endif
+
+#define STDMETHODCALLTYPE       __stdcall
+#define STDMETHODVCALLTYPE      __cdecl
+
+#define STDAPICALLTYPE          __stdcall
+#define STDAPIVCALLTYPE         __cdecl
+
+#define STDAPI                  EXTERN_C __export HRESULT __stdcall
+#define STDAPI_(type)           EXTERN_C __export type __stdcall
+
+#define STDMETHODIMP            HRESULT STDMETHODCALLTYPE
+#define STDMETHODIMP_(type)     type STDMETHODCALLTYPE
+
+#define STDOVERRIDEMETHODIMP        __override STDMETHODIMP
+#define STDOVERRIDEMETHODIMP_(type) __override STDMETHODIMP_(type)
+
+#define IFACEMETHODIMP          __override STDMETHODIMP
+#define IFACEMETHODIMP_(type)   __override STDMETHODIMP_(type)
+
+#ifndef interface
+#define interface struct
+#endif
+
+#define STDMETHOD(method)        virtual HRESULT STDMETHODCALLTYPE method
+#define STDMETHOD_(type,method)  virtual type STDMETHODCALLTYPE method
+#define STDMETHODV(method)       virtual HRESULT STDMETHODVCALLTYPE method
+#define STDMETHODV_(type,method) virtual type STDMETHODVCALLTYPE method
+#define PURE                    = 0
+#define THIS_
+#define THIS                    void
+#define IFACEMETHOD(method)         __override STDMETHOD(method)
+#define IFACEMETHOD_(type,method)   __override STDMETHOD_(type,method)
+#define IFACEMETHODV(method)        __override STDMETHODV(method)
+#define IFACEMETHODV_(type,method)  __override STDMETHODV_(type,method)
+
+
+// -------------------------------------------------------------------------- //
+
+#endif// __PRE_UNIXDEF_H__
